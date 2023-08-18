ee889f1b9d2a6516aefbb9dd593c383f1af167e0
Ilya Kolesnik <ilya-kolesnic@yandex.ru>
2023-08-18 20:48:00 +0300

    Second commit

--- docker-compose/appdata/simpleweb-main/current_handlers.py
+++ docker-compose/appdata/simpleweb-main/current_handlers.py
@@ -4,12 +4,14 @@ import pyodbc
 def sql_test(hashMap,_files=None,_data=None):
     
     cnxn = pyodbc.connect("DRIVER={/opt/microsoft/msodbcsql18/lib64/libmsodbcsql-18.3.so.1.1};Server=192.168.1.96;UID=sa;PWD=PLC_@ccess_test_1;Database=msdb;TrustServerCertificate=yes;")
-    df = pd.read_sql_query('select part_id from dbo.tbl_pallets WHERE RFID_id = 1', cnxn)
-    sdf = str(df.loc[0]) 
-    print(f'{sdf=}')   
+    df = pd.read_sql_query('select part_id from dbo.tbl_pallets WHERE RFID_id = 4', cnxn)
+    #print(f'{df.to_dict()=}')   
+    #print(f'{df.loc[0].to_dict()=}')   
+    #print(f'{df.loc[0].at["part_id"]=}')   
+    #sdf = str(df.loc[0].to_dict())
     #print(pd.read_sql_query('select part_id from dbo.tbl_pallets WHERE RFID_id = 1', cnxn))
-    if sdf:    
-        hashMap.put("toast",sdf) 
+    if df.size:    
+        hashMap.put("toast",df.loc[0].at["part_id"]) 
     else:
         hashMap.put("toast","Ашипка")             
     
